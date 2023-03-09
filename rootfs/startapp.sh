5f6199ab1e25d86ef5832c7fd76b0a8120ea1148
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-03-09 14:06:59 -0500

    Export the `GTK_A11Y=none` variable to avoid warning message from GTK.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 export HOME=/config
+
+# This is to avoir the following warning:
+#   (czkawka_gui:613): Gtk-WARNING **: 17:45:24.876: Unable to acquire the address of the accessibility bus: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name org.a11y.Bus was not provided by any .service files. If you are attempting to run GT
+export GTK_A11Y=none
+
 cd /storage
 exec /usr/bin/czkawka_gui
