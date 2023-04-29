9d713778e50ef24bd57369ac13e074244574f9b2
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-04-29 14:27:19 -0400

    Fixed typo in comment.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export HOME=/config
 
-# This is to avoir the following warning:
+# This is to avoid the following warning:
 #   (czkawka_gui:613): Gtk-WARNING **: 17:45:24.876: Unable to acquire the address of the accessibility bus: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name org.a11y.Bus was not provided by any .service files. If you are attempting to run GT
 export GTK_A11Y=none
 
